729f76739ee5bb2d04e5958d5bd5e4b51b309589
graziacrp <graziacarpineta25@gmail.com>
2022-11-16 17:02:48 +0100

    ok

--- parole palindrome o no/script.js
+++ parole palindrome o no/script.js
@@ -1,8 +1,7 @@
 //Chiedo all'utente di inserire una parola
 let parolaIniziale = prompt("Inserisci una parola");
 
-//Chiedo all'utente di inserire la parola al contrario
-let parolaFinale = prompt("Inserisci la parola al contrario");
+
 
 
 //Verifico se la parola sia palindroma o meno
