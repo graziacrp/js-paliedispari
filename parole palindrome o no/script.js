96cbf6ad93c917cbaeda3664700fa015ff139fab
graziacrp <graziacarpineta25@gmail.com>
2022-11-16 17:12:34 +0100

    boh

--- parole palindrome o no/script.js
+++ parole palindrome o no/script.js
@@ -3,9 +3,20 @@ let parolaIniziale = prompt("Inserisci una parola");
 
 
 
+// Inverto la parola impiccandomi come sempre
+let invertiparolaIniziale = '';
+
+for (let i = parolaIniziale.length - 1; i >= 0; i--) {
+    invertiparolaIniziale += parolaIniziale.charAt(i); 
+}
+console.log(`La parola al contrario è: ${invertiparolaIniziale}`);
+
+
+
+
 
 //Verifico se la parola sia palindroma o meno
-if (parolaIniziale == parolaFinale) {
+if (parolaIniziale == invertiparolaIniziale) {
     alert('La parola è palindroma');
 
 } else {
