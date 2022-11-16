7b8d13cfcac8531a09e3459796fef5b5923db8b5
graziacrp <graziacarpineta25@gmail.com>
2022-11-16 16:21:57 +0100

    ok

--- pari o dispari/script.js
+++ pari o dispari/script.js
@@ -1,16 +1,19 @@
 //Chiedo all'utente di scegliere un numero pari o dispari
 let pariODispari = prompt("Vuoi un numero pari o dispari?");
 
-console.log ("La tua scelta è:", pariODispari);
-
-let numeroUtente = parseInt (prompt("Inserisci un numero da 1 a 5"));
-
+//Stampo il numero
+console.log("La tua scelta è:", pariODispari);
 
 
+//Chiedo all'utente di inserire un numero da 1 a 5
+let numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
 
 
 
-//Generare un numero casuale da 1 a 5 usando la funzione
+
+
+
+//Genero un numero casuale da 1 a 5 usando la funzione
 function casuale() {
     num = Math.round(Math.random() * 5);
     document.getElementById("casuale").innerHTML = "Numero casuale " + num;
@@ -19,46 +22,48 @@ function casuale() {
 
 Math.round(Math.random() * 4) + 1;
 
-
-//Stabiliamo se il numero è pari o dispari 
-
-function numeropari(pariODispari) {
-    if (pariODispari % 2 == 0) {
-        risultato = vero;
-    } else {
-        risultato = falso;
-    }
-}
-
-if (numeropari(somma) === pariOdispari) {
-
-    alert("hai vinto");
-} else {
-
-    alert("hai perso");
-}
-
-
-
-
-
-
-
-
-
-
 //Sommo i due numeri
 
-let somma = sceltaUtente + numeroCasuale();
+let somma = numeroUtente + casuale();
 console.log(somma);
 
-//Se l'utente sceglie un numero uguale alla funzione numeroPpri, ha vinto. altrimenti ha perso   (?)
 
-if (sceltaUtente == numeroFinale(somma)) {
-    console.log("Congratulazioni, hai vinto!");
-} else {
-    console.log("Mi dispiace, hai perso..");
+//Stabilisco se la somma dei due numeri è pari o dispari (usando la funzione)
+
+function pari(numero) {
+
+    let risultato = "dispari";
+    if (numero % 2 == 0) {
+        risultato = "pari";
+        console.log("il numero risultato è" + numero);
+    }
+
+    return risultato;
 }
+
+//Dichiaro chi ha vinto
+
+if (pari(somma) == pariODispari) {
+
+    alert("Congratulazioni, hai vinto!");
+} else {
+
+    alert("Mi dispiace, hai perso!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
