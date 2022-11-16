d48a0de66bb4fe64f329b273bc15e4fb70f990f9
graziacrp <graziacarpineta25@gmail.com>
2022-11-16 14:09:22 +0100

    ok

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let parola = prompt("Inserisci una parola");
+//Chiedo all'utente di inserire una parola
+let parolaIniziale = prompt("Inserisci una parola");
 
-if (parola == parola) {
+//Chiedo all'utente di inserire la parola al contrario
+let parolaFinale = prompt("Inserisci la parola al contrario");
+
+
+//Verifico se la parola sia palindroma o meno
+if (parolaIniziale == parolaFinale) {
     alert('La parola è palindroma');
 
 } else {
