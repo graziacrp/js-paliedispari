0e6b7e8f4bd32b4b21bd9a71a6aee47305c76318
graziacrp <graziacarpineta25@gmail.com>
2022-11-16 14:48:44 +0100

    ok

--- pari o dispari/script.js
+++ pari o dispari/script.js
@@ -1,5 +1,13 @@
 //Chiedo all'utente di scegliere un numero pari o dispari
-let evenOrOdd = prompt("Vuoi un numero pari o dispari?");
+let pariODispari = prompt("Vuoi un numero pari o dispari?");
 
+//Devo capire se l'utente ha scelto un numero pari o dispari, quindi creo una variabile
 
+let sceltaUtente;
+
+if (pariODispari == "pari") {
+    sceltaUtente = vero;
+} else if (pariODispari == "dispari") {
+    sceltaUtente = falso;
+}
 
