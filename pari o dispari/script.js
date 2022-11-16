151598726c682a960efd1df05edc00ac63b7d0f8
graziacrp <graziacarpineta25@gmail.com>
2022-11-16 14:34:07 +0100

    ok

--- pari o dispari/script.js
+++ pari o dispari/script.js
@@ -1,3 +1,5 @@
 //Chiedo all'utente di scegliere un numero pari o dispari
-const evenOrOdd = prompt("Vuoi un numero pari o dispari?");
+let evenOrOdd = prompt("Vuoi un numero pari o dispari?");
+
+
 
